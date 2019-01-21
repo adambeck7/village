1e74a47089da610e7e58c9f5ab6d799af17d91bd
adambeck7 <adambeck7@gmail.com>
2019-01-21 13:19:14 -0700

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,13 +1,39 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+import Vue from 'vue';
+import './registerServiceWorker';
+// import './plugins/vuetify';
+import App from './App.vue';
+import router from './router';
+// import store from './store';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+// import 'roboto-fontface/css/roboto/roboto-fontface.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+// new Vue({
+//   router,
+//   // store,
+//   render: h => h(App)
+// }).$mount('#app');
+let app = '';
+// Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyDcxyKhZYNSAEcxNEA7azM8Gx4oigo6gHI',
+  authDomain: 'village-messaging.firebaseapp.com',
+  databaseURL: 'https://village-messaging.firebaseio.com',
+  projectId: 'village-messaging',
+  storageBucket: 'village-messaging.appspot.com',
+  messagingSenderId: '1054601568102'
+};
+firebase.initializeApp(config);
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    /* eslint-disable no-new */
+    app = new Vue({
+      router,
+      render: h => h(App)
+    }).$mount('#app');
+  }
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img class="home-logo" alt="Village logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Village"/>
+    <button @click="logout">Logout</button>
   </div>
 </template>
 
 <script>
+import firebase from "firebase";
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import HelloWorld from "@/components/HelloWorld.vue";
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      uid: "",
+      email: "",
+      displayName: "",
+      active: false
+    };
+  },
+  created: function() {
+    this.db = firebase.auth().currentUser;
+    this.uid = this.db.uid;
+    this.email = this.db.email;
+    this.displayName = this.db.displayName;
+  },
+  methods: {
+    logout: function() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.$router.replace("login");
+        });
+    }
   }
-}
+};
 </script>
+
+<style>
+.home-logo {
+  max-width: 10em;
+}
+</style>
