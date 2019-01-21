faccc62264203141632d6e8933cb023dcdda9caa
adambeck7 <adambeck7@gmail.com>
2019-01-21 15:55:29 -0700

    added vuetify, nav drawer, and inbox

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,82 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
+      <v-layout>
+        <v-container>
+          <v-layout justify-start>
+            <!-- <v-btn color="pink" dark @click.stop="drawer = !drawer">Toggle</v-btn> -->
+            <v-btn fab dark color="teal" @click.stop="drawer = !drawer">
+              <v-icon dark>list</v-icon>
+            </v-btn>
+          </v-layout>
+          <img class="home-logo" alt="Village logo" src="./assets/logo.png">
+        </v-container>
+
+        <v-navigation-drawer v-model="drawer" absolute temporary>
+          <v-list class="pa-1">
+            <v-list-tile avatar class="avatar-padding">
+              <!-- <v-list-tile-avatar>
+                <img src="https://randomuser.me/api/portraits/men/85.jpg">
+              </v-list-tile-avatar>-->
+              <v-avatar color="teal">
+                <span class="white--text headline">A</span>
+              </v-avatar>
+
+              <v-list-tile-content>
+                <v-list-tile-title style="padding-left:1.3em">Adam Beck</v-list-tile-title>
+                <button @click="logout" style="padding-left:1.3em">Logout</button>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+
+          <v-list class="pt-0" dense>
+            <v-divider></v-divider>
+
+            <v-list-tile v-for="item in items" :key="item.title" @click>
+              <v-list-tile-action>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-tile-action>
+
+              <v-list-tile-content>
+                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-navigation-drawer>
+      </v-layout>
+      <!-- <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>-->
     </div>
     <router-view/>
   </div>
 </template>
+
+
+<script>
+import firebase from "firebase";
+export default {
+  data() {
+    return {
+      drawer: null,
+      items: [
+        { title: "Home", icon: "dashboard" },
+        { title: "About", icon: "question_answer" }
+        // { title: "Logout", icon: "mdi-logout" }
+      ]
+    };
+  },
+  methods: {
+    logout: function() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.$router.replace("login");
+        });
+    }
+  }
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -17,7 +87,7 @@
   color: #2c3e50;
 }
 #nav {
-  padding: 30px;
+  // padding: 30px;
   a {
     font-weight: bold;
     color: #2c3e50;
@@ -26,4 +96,18 @@
     }
   }
 }
+.home-logo {
+  max-width: 10em;
+  margin-top: -3em;
+}
+.avatar-padding > :first-child {
+  padding-left: 0 !important;
+}
 </style>
+
+<style scoped>
+/* .v-list__tile__content {
+  padding-left: 1.3em;
+} */
+</style>
+
